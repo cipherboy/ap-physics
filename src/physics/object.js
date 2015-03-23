bc673c1d01dd8ef9b79373f766d87c4c5857cb94
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-03-23 14:27:58 -0500

    Solenoid stuffs, initial.

---
+++ src/physics/object.js
@@ -0,0 +1,8 @@
+function Player() {
+  this.charge = 0;
+  this.position;
+  
+  this.init = function() { };
+  
+  this.getSpeed = function() {  };
+}
---
+++ src/physics/solenoid.js
@@ -0,0 +1,93 @@
+function solenoid() {
+  this.length = 0;
+  this.coils = 0;
+  this.center = [0, 0];
+  this.angle = 0;
+  this.radius = 0;
+  this.current = 0;
+  this.color = '#000';
+  
+  /**
+   * Return frame
+  **/
+  this.frame = function() {
+    var frame = 'b,ss:' + this.color + ',';
+    return frame;
+  };
+
+  /** 
+   * Given object
+   * Return force on object
+  **/
+  this.force = function(object) 
+    if (this.contains(object)) {
+      return [4 * Math.PI object.charge * object.getSpeed() * this.coils/this.length * this.current, this.angle];
+    } else {
+      return [4 * Math.PI object.charge * object.getSpeed() * this.coils/this.length * this.current / Math.pow(this.distance(object), 2), this.angle];
+    }
+  };
+
+  /** 
+   * Given object
+   * Return true if contains || false
+   *
+   * Left, Right, Top, Bottom checking (best case: left of object. worst case: below object)
+  **/
+  this.contains = function(object) {
+    var bounds = this.bounds();
+    var left = ( bounds[0][1] - bounds[3][1] ) / ( bounds[0][0] - bounds[3][1] );
+    var right = ( bounds[1][1] - bounds[2][1] ) / ( bounds[1][0] - bounds[2][1] );
+    var top = ( bounds[1][1] - bounds[0][1] ) / ( bounds[1][0] - bounds[0][1] );
+    var bottom = ( bounds[2][1] - bounds[3][1] ) / ( bounds[2][0] - bounds[3][1] );
+    
+    if ( left * (object.center[0] - bounds[0][0]) < object.center[1] - bounds[0][1]) {
+      return false;
+    } else if ( right * (object.center[0] - bounds[1][0]) > object.center[1] - bounds[1][1]) {
+      return false;
+    } else if ( top * (object.center[0] - bounds[0][0]) < object.center[1] - bounds[0][1]) {
+      return false;
+    } else if ( bottom * (object.center[0] - bounds[2][0]) > object.center[1] - bounds[2][1]) {
+      return false;
+    }
+    
+    return true;
+  };
+
+  /**
+   * Return bounds of object according to the following chart.
+   * 
+   *     2l
+   *     ^
+   *   l   l
+   * 0-------1
+   * |       | r
+   * |   c   |   } 2r
+   * |       | r
+   * 3-------2
+  **/
+  this.bounds = function() {
+    var transform = [
+      rotate([-1*this.length, r], this.angle),
+      rotate([this.length, r], this.angle),
+      rotate([this.length, -r], this.angle),
+      rotate([-1*this.length, r], this.angle),
+    ];
+    
+    return [
+      [center[0] + transform[0][0], center[1] + transform[0][1]],
+      [center[0] + transform[1][0], center[1] + transform[1][1]],
+      [center[0] + transform[2][0], center[1] + transform[2][1]],
+      [center[0] + transform[3][0], center[1] + transform[3][1]]
+    ];
+  };
+
+  /**
+   * Given [x, y] and angle,
+   * Return rotation about origin
+   *
+   * Note: if center != origin, add after
+  **/
+  this.rotate = function(points, angle) {
+    return [ points[0] * Math.cos(angle) - points[1] * Math.sin(angle) , points[0] * Math.sin(angle) + points[1] * Math.sin(angle) ];
+  };
+};
