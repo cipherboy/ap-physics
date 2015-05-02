5f5370039c8b385a98b85dfcd1289cc65a66432f
Alex Scheel <alexander.m.scheel@gmail.com>
2015-05-02 12:37:22 -0500

    Working solenoid drawing. Finally!

--- src/physics/solenoid.js
+++ src/physics/solenoid.js
@@ -28,16 +28,15 @@ function Solenoid() {
    *        Quantity of loops unrelated to coil count; starts from bound[0].
   **/
   this.frame = function() {
-    var frame = 'b,sa,ss:' + this.color + ',sa,';
-    var corner = this.bounds()[0];
-    var working = this.radius/4;
-    var increment = working/100;
-    var end = this.length/working + ((this.length/working) % (2*Math.PI));
-    //frame += 'm:' + corner[0] + ':' + corner[1] + ',';
-    for (var t = increment; t < end; t += increment) {
-      frame += 'l:' + (corner[0] + (working * t) - (this.radius * Math.sin(t))) + ':' + (corner[1] + working - (this.radius * Math.cos(t))) + ',';
+    var frame = 'b,ss:' + this.color + ',sa,'
+    var corner = [this.center[0] - this.length/2, this.center[1] - this.radius];
+    var working = this.radius/6;
+    var increment = working/50;
+    for (var t = increment; t < this.length/working; t += increment) {
+      var points = this.rotate([((corner[0]) + (working * t) - (this.radius * Math.sin(t))), ((corner[1] + this.radius) - (this.radius * Math.cos(t)))], this.angle);
+      frame += 'l:' + points[0] + ':' + points[1] + ',';
     }
-    frame += 'r:' + this.angle + ',w:' + this.width + ',s,re,c';
+    frame += ',w:' + this.width + ',s,re,c';
     return frame;
   };
 
@@ -112,18 +111,11 @@ function Solenoid() {
    * 3-------2
   **/
   this.bounds = function() {
-    var transform = [
-      this.rotate([-1*this.length, this.radius], this.angle),
-      this.rotate([this.length, this.radius], this.angle),
-      this.rotate([this.length, -1 * this.radius], this.angle),
-      this.rotate([-1*this.length, -1*this.radius], this.angle),
-    ];
-    
     return [
-      [this.center[0] + transform[0][0], this.center[1] + transform[0][1]],
-      [this.center[0] + transform[1][0], this.center[1] + transform[1][1]],
-      [this.center[0] + transform[2][0], this.center[1] + transform[2][1]],
-      [this.center[0] + transform[3][0], this.center[1] + transform[3][1]]
+      this.rotate([this.center[0] - this.length/2, this.center[1] - this.radius], this.angle),
+      this.rotate([this.center[0] + this.length/2, this.center[1] - this.radius], this.angle),
+      this.rotate([this.center[0] + this.length/2, this.center[1] + this.radius], this.angle),
+      this.rotate([this.center[0] - this.length/2, this.center[1] + this.radius], this.angle),
     ];
   };
 
@@ -134,8 +126,10 @@ function Solenoid() {
    * Note: if center != origin, add after return.
   **/
   this.rotate = function(points, angle) {
-    var radius = Math.sqrt(Math.pow(points[0], 2), Math.pow(points[1], 2));
-    var start = Math.atan(points[1] / points[0]);
-    return [ radius * Math.cos(start + angle) , radius*Math.sin(start + angle) ];
+    var x = points[0] - this.center[0]; 
+    var y = points[1] - this.center[1];
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    return [ x*cos + y*sin + this.center[0], -x*sin + y*cos + this.center[1] ];
   };
 };
