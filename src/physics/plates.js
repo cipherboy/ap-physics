54cd1cf0954c845e588701ad17a496eb4bcb84e5
katmayo7 <katherine.y.mayo@gmail.com>
2015-05-03 20:56:32 -0500

    Calculations
    Calculations for plate and initial set up

--- src/physics/plates.js
+++ src/physics/plates.js
@@ -1,21 +1,49 @@
 /**
- * Creates electric plates with either a + or - charge
- * calculates the force on the object
- * gives charge given to the object
+* Creates plate objects to draw and calculate forces from it
 **/
-function Plates() {
-  this.length = 0;
-  this.charge = 0;
-  this.force = function() { };
 
-  /**
-   * Generic initialization function
-  **/
-  this.init = function(obj) {
-    for (place in obj) {
-      if (place && place in this) {
-        this[place] = obj[place];
-      }
-    }
-  };
-}
+function Plates()
+{
+    this.length = 0;
+    this.charge = 0;
+    this.color = '#000';
+    
+    /**
+    * Generic initialization function
+    **/
+    
+    this.init = function(obj)
+    {
+        for(place in obj)
+        {
+            if(place && place in this)
+            {
+                this[place] = obj[place];
+            }
+        }
+    };
+    
+    /**
+    * Return drawn frame.
+    **/
+    
+    this.frame = function()
+    {
+    };
+    
+    /**
+    * Given object, return electric field acting on object
+    **/
+    
+    /** 
+    * Equations: 
+    * E = Q/ EA (E = 8.85 x 10^-12)
+    * W = Eqr
+    **/ 
+    
+    this.field = function(object)
+    {
+        var perm = 8.85E-12;
+        return object.charge / [object.length * object.length * perm];
+    };
+};
