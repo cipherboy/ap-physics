68f6d6078e642afd73cc2b460be611c0491283c9
katmayo7 <katherine.y.mayo@gmail.com>
2015-05-11 11:13:20 -0500

    grounding .js and test

--- src/physics/grounding.js
+++ src/physics/grounding.js
@@ -3,6 +3,7 @@ function Grounding() {
   this.angle = 0;
   this.radius = 0;
   this.wdith = 0;
+  this.charge = 0;
   this.center = [0, 0];
   this.color = '#000';
 
@@ -21,16 +22,48 @@ function Grounding() {
    * Return drawn frame
   **/
   this.frame = function() {
-    var frame = 'b,sa,ss:' + this.color + ',r:' + this.angle + 'w:' + this.width + ",";
-    var corners = this.bounds();
-    from += 'm:' + corners[0][0] + ':' + corners[0][1] + ',l:' + corners[1][1] + ',';
-    frame += 's,re,c';
+    var frame = 'b,ss:' + this.color + ',w:' + this.width + ",";
+    var corners = this.bounds(); // + rotation
+    frame += 'm:' + corners[0][0] + ':' + corners[0][1] + ',l:' + corners[1][0] + ':' + corners[1][1] + ',';
+    var mark = this.width * 4;
+    for (var start = this.center[0] - this.length/2; start < this.center[0] + this.length/2; start += mark * 2) {
+      var end = start + mark;
+      var startv = this.rotate([start, this.center[1] + this.radius], this.angle);
+      var endv = this.rotate([end, this.center[1] + this.radius], this.angle);
+      frame += 'm:' + startv[0] + ':' + startv[1] + ',l:' + endv[0] + ':' + endv[1] + ',';
+    }
+    if (this.charge >= 0) {
+      for (var start = this.center[0] - this.length/2; start < this.center[0] + this.length/2; start += mark * 2) {
+        var startv = this.rotate([start+mark/2, this.center[1] + this.radius - mark], this.angle);
+        var endv = this.rotate([start+mark/2, this.center[1] + this.radius + mark], this.angle);
+        frame += 'm:' + startv[0] + ':' + startv[1] + ',l:' + endv[0] + ':' + endv[1] + ',';
+      }
+    }
+    frame += 's,c';
+    return frame;
   };
 
+/** 
+* Makes the field equal to 0 aka, grounds it
+**/
+    
   this.field = function(object) {
     return 0;
   };
-
+    
+    /**
+    *Return bounds of object according to the following chart.
+    *
+    *        l
+    *        ^
+    *  1/2      1/2
+    * 0________1
+    * |        | r
+    * |    c   |   } 2r
+    * |        | r
+    * 3--------2
+    **/
+    
   this.bounds = function() {
     return [
       this.rotate([this.center[0] - this.length/2, this.center[1] - this.radius], this.angle),
